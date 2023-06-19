50a225d6c132643e5f91c43af6fc69054e03e678
Anshen <plouisin@gmail.com>
2023-06-19 20:23:40 +0200

    car catalog && rapid api car

--- components/index.ts
+++ components/index.ts
@@ -2,10 +2,20 @@ import Hero from './Hero';
 import CustomButton from './CustomButton';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import SearchBar from './SearchBar';
+import CustomFilter from './CustomFilter';
+import SearchManufacturer from './SearchManufacturer';
+import CardCard from './CardCard';
+import CarDetails from './CarDetails';
 
 export {
   Hero,
   CustomButton,
   Navbar,
   Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+  CardCard,
+  CarDetails
 }
