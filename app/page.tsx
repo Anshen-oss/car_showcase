2ae6a60dccda98585965477135012f5b7556bf73
Anshen <plouisin@gmail.com>
2023-06-26 21:39:46 +0200

    show more

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 
-import { Hero, SearchBar, CustomFilter, CarCard } from '@/components'
+import { Hero, SearchBar, CustomFilter, CarCard, ShowMore } from '@/components'
 import { fetchCars } from '@/utils';
 import { fuels, yearsOfProduction } from '@/constants';
 
@@ -42,6 +42,11 @@ export default async function Home({ searchParams }) {
             <div className="home__cars-wrapper">
               {allCars.map((car) => (<CarCard car={car} />))}
             </div>
+
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
           </section>
         ) : (
           <div className="home__error-container">
